c3c49597d39e9b39d0cbc26880eca65adeedb596
zxxex <s1zycc@gmail.com>
2025-12-17 14:40:12 +0800

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC<HeaderProps> = ({
                 <ThemeSwitcher />
                 <LanguageSwitcher />
                 <a
-                  href="https://github.com/zaunist/zmail"
+                  href="https://github.com/zaunist/ZeYu"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="w-8 h-8 flex items-center justify-center rounded-md transition-all duration-200 hover:bg-primary/20 hover:text-primary hover:scale-110 ml-1"
